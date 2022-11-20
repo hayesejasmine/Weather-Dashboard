9a698c7f538d047ca0ada0ed572d24ed45cb7d24
hayesejasmine <hayesejasmine@gmail.com>
2022-11-20 13:55:36 -0500

    added API key and eventlistener to JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,18 @@
-const options = {
-    method: 'GET',
-    headers: {
-        APIKey: "024d7fc6bc6f29f1df64b23b920121d3"
 
-    }
-};
+const apiKey = "024d7fc6bc6f29f1df64b23b920121d3"
+function getWeatherForecast(cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            console.log(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
+        })
+        .catch(err => console.error(err));
+}
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid')
-.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+
+var searchBtn = document.getElementById("searchButton") 
+searchBtn.addEventListener("click",function(){
+    var city = document.getElementById("cityName").value
+    getWeatherForecast(city)
+})
